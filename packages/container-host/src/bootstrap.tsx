e690d70a6443d7e40ba0bc47e13b16eb5b29920c
eviterif <eloy.viteri@skookum.com>
2022-02-15 21:33:28 -0500

    updated container

--- packages/container-host/src/bootstrap.tsx
+++ packages/container-host/src/bootstrap.tsx
@@ -1,28 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { Router } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
 
-const history = createBrowserHistory()
-
-const remoteMount = (element: HTMLElement) => {
-  ReactDOM.render(
-    <Router history={history}>
-      <App />
-    </Router>,
-    element
-  )
-}
-
-// If we are in development and in isolation,
-// call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.getElementById('root')
-
-  if (devRoot) {
-    remoteMount(devRoot)
-  }
-}
-
-export { remoteMount }
+ReactDOM.render(<App />, document.querySelector('#root'))
